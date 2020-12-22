11639518f195e8903ba92dccd62011692ea3a961
nkanyi73 <nkanyi73@gmail.com>
2020-12-22 20:50:40 +0300

    Initial commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,17 @@
+@extends ('layouts.app')
+
+@section('content')
+<div class ="container">
+    <div class="row justify-content-center">
+        <h2 class="display-6">Welcome</h2>
+        <div class="card-group">
+            <div class="card">
+            <img class="card-img-top" src="" alt="Swimming">
+                <div class="card-body">
+                    <div class="card-title">Swimming</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
